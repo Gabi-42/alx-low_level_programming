c3c6b317df15b0d3b08b62afe2915ed309432a01
Gabriella Peji <omotpeji@gmail.com>
2022-07-08 15:24:33 -0700

    tasks

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * followed by a newline
+ * Returns: returns 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0, i < 0, i++)
+		_putchar(i + '0');
+	_putchar('\n');
+}
