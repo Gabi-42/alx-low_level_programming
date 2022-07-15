c265f855a45ecc90533bd9c6c374046fab098d29
Gabriella Peji <omotpeji@gmail.com>
2022-07-15 10:52:48 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,7 +14,7 @@ char *_strncat(char *dest, char *src, int n)
 		dest_len++;
 
 	for (index = 0; src[index] && index < n; index++)
-		dest[desst_len++] = src[index];
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
