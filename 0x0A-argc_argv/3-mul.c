5283882b62d9a716289b4f06896bf96e88b4490d
Gabriella Peji <omotpeji@gmail.com>
2022-07-26 15:14:14 -0700

    alx task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that multiplies two numbers.
+ * @argc: number of arguments
+ * @argv: vector of arguments
+ *
+ * Return: 0 on Success
+ */
+int main(int argc, char *argv[])
+{
+	int index, multiplication;
+
+	multiplication = 1;
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (index = 1; index < argc; index++)
+	{
+		multiplication = multiplication * atoi(argv[index]);
+	}
+	printf("%d\n", multiplication);
+	return (0);
+}
